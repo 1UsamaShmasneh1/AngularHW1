f02a51c9c6ae0b33d4a0ba02f9aded6f094f40d1
usama shmasneh <us.05.07.91@gmail.com>
2022-03-11 00:31:44 +0200

    check

--- src/app/pic/pic.component.ts
+++ src/app/pic/pic.component.ts
@@ -9,6 +9,8 @@ export class PicComponent implements OnInit {
 
   constructor() { }
 
+  imgSRC:string = 'https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.giant-bicycles.com%2Fus%2Ftalon-2&psig=AOvVaw1f1M7brVYAjlpTXgvTyKAG&ust=1647037108668000&source=images&cd=vfe&ved=0CAsQjRxqFwoTCMC-87_JvPYCFQAAAAAdAAAAABAS';
+
   ngOnInit(): void {
   }
 
